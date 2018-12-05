1534122190d4b40a9ce8fde9697e27de544d888f
Arthur Terteryan <arthur.terteryan@gmail.com>
2018-12-05 23:33:35 +0300

    Add fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
-        this.state = { searchValue: '', robots };
+        this.state = { searchValue: '', robots: [] };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearch = (event) => {
@@ -16,16 +21,18 @@ class App extends Component {
 
     filteredRobots() {
         const { robots, searchValue } = this.state;
-        
+
         return robots.filter(robot => robot.name.toLowerCase().includes(searchValue.toLowerCase()));
     }
 
     render() {
-        return <div className='tc'>
-            <h1 className='f1'>ROBOAPP</h1>
-            <SearchBox search={this.onSearch} />
-            <CardList robots={this.filteredRobots()} />
-        </div>;
+        return !this.state.robots.length ?
+            <h1>Loading...</h1> :
+            <div className='tc'>
+                <h1 className='f1'>ROBOAPP</h1>
+                <SearchBox search={this.onSearch} />
+                <CardList robots={this.filteredRobots()} />
+            </div>;
     }
 }
 
